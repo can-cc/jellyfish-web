d9454ef29b9e2dac866441016c23194e00df028b
fwchen <crystal.tyan@gmail.com>
2018-04-25 21:06:47 +0800

    axios helper

---
+++ src/helper/interceptor.helper.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { history } from '../history';
+
+function responseSuccessInterceptor(response) {
+  return response;
+}
+
+function responseFailureInterceptor(error) {
+  if (error.response.status === 401) {
+    window.localStorage.removeItem('jwt');
+    history.push('/login');
+  }
+  return Promise.reject(error);
+}
+
+function setupAxiosInterceptor() {
+  axios.interceptors.response.use(responseSuccessInterceptor, responseFailureInterceptor);
+}
+
+function setupAxiosJwtHeader(jwt: string) {
+  axios.defaults.headers.common['jwt'] = window.localStorage.getItem('jwt');
+}
+
+export function setRequestAuth() {
+  setupAxiosInterceptor();
+  setupAxiosJwtHeader();
+}
