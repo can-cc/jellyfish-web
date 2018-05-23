8319fef749ae698e83a395b75f38989b077ce764
fwchen <crystal.tyan@gmail.com>
2018-05-23 23:04:59 +0800

    update

---
+++ src/page/SignUp/SignUp.js
@@ -0,0 +1,33 @@
+// @flow
+import React, { Component } from 'react';
+import axios from 'axios';
+import Button from 'antd/lib/button';
+import { SignInForm } from './SignInForm';
+import { BrowserRouter as Router, Route, Link, Redirect, withRouter } from 'react-router-dom';
+
+import './SignUp.scss';
+
+export const SignIn = withRouter(
+  class extends Component<{
+    history: RouterHistory
+  }> {
+    signIn = async (data: SignInFormData) => {
+      try {
+        const resp: AxiosXHR<{ token: string, id: string }> = await axios.post('/api/signin', data);
+        window.localStorage.setItem('jwt', resp.data.token);
+        window.localStorage.setItem('userId', resp.data.id);
+
+        this.props.history.push('/todo');
+      } catch (error) {}
+    };
+
+    render() {
+      return (
+        <div>
+          <img className="signin-logo" alt="logo" src="/assets/imgs/logo.png" />
+          <SignInForm submit={this.signIn} />
+        </div>
+      );
+    }
+  }
+);
