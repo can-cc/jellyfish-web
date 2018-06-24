4a976e795ed4d1369c64fd30ee3e94a017231590
fwchen <crystal.tyan@gmail.com>
2018-06-24 15:22:30 +0800

    update

--- src/page/Todo/TodoList.js
+++ src/page/Todo/TodoList.js
@@ -1,21 +1,19 @@
 // @flow
 import React, { Component } from 'react';
-import Collapse from 'antd/lib/collapse';
 import List from 'antd/lib/list';
-
 import { TodoItem } from './TodoItem';
 
-const Panel = Collapse.Panel;
-
 class TodoCollection extends Component<{ todos: any[], onTodoChange: any }, {}> {
-  return() {
-    <List
-      size="large"
-      header={null}
-      footer={null}
-      dataSource={this.props.todos}
-      renderItem={todo => <TodoItem todo={todo} onChange={this.props.onTodoChange} />}
-    />;
+  render() {
+    return (
+      <List
+        size="large"
+        header={null}
+        footer={null}
+        dataSource={this.props.todos}
+        renderItem={todo => <TodoItem todo={todo} onChange={this.props.onTodoChange} />}
+      />
+    );
   }
 }
 
@@ -24,18 +22,6 @@ export class TodoList extends Component<{ todos: any[], onTodoChange: any }> {
     return (
       <div>
         <TodoCollection todos={this.props.todos} onTodoChange={this.props.onTodoChange} />
-        <Collapse>
-          <Panel header="This is panel header 1" key="1">
-            <List
-              size="large"
-              header={null}
-              footer={null}
-              bordered
-              dataSource={this.props.todos}
-              renderItem={todo => <TodoItem todo={todo} onChange={this.props.onTodoChange} />}
-            />
-          </Panel>
-        </Collapse>
       </div>
     );
   }
