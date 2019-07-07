cd21628591dd7a410d3166975795bf2bf1fdf2a2
fwchen <me@chenfangwei.com>
2019-07-07 22:42:12 +0800

    change proxy address port

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,11 +3,11 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     proxy('/api', {
-      target: 'http://localhost:8000/',
+      target: 'http://localhost:8180/',
       pathRewrite: {
         '^/api/': '/'
       }
     })
   );
-  app.use(proxy('/upload', { target: 'http://localhost:8000/' }));
+  app.use(proxy('/upload', { target: 'http://localhost:8180/' }));
 };
