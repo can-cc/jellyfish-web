833c2fdd2e46ef99bc564959cc9673745bf96085
chchen <chchen@thoughtworks.com>
2020-10-25 22:32:09 +0800

    update

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,14 +1,29 @@
 /* eslint-disable */
 
+import { CacheFirst } from 'workbox-strategies/CacheFirst';
+import { NetworkFirst } from 'workbox-strategies/NetworkFirst';
+import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+import { registerRoute } from 'workbox-routing/registerRoute';
+
 precacheAndRoute(self.__WB_MANIFEST);
 
-workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
+registerRoute('/', NetworkFirst());
+
+registerRoute(
+  '/static/',
+  new CacheFirst({
+    cacheName: 'static-cache',
+    matchOptions: {
+      ignoreVary: true
+    }
+  })
+);
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-
-// We need this in Webpack plugin (refer to swSrc option): https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin#full_injectmanifest_config
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
-
-// app-shell
-workbox.routing.registerRoute('/', workbox.strategies.networkFirst());
