782684be87a715bd4af27c449e13fce9ffdea257
chchen <chchen@thoughtworks.com>
2020-10-25 21:58:00 +0800

    update

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,36 @@
-const { rewireWorkboxInject, defaultInjectConfig } = require('react-app-rewire-workbox');
 const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+function findSWPrecachePlugin(element) {
+  return element.constructor.name === 'GenerateSW';
+}
+
+function removeSWPrecachePlugin(config) {
+  const swPrecachePluginIndex = config.plugins.findIndex(findSWPrecachePlugin);
+  if (swPrecachePluginIndex !== -1) {
+    config.plugins.splice(swPrecachePluginIndex, 1); // mutates
+  }
+}
+
+// function rewireWorkboxInject(workboxConfig) {
+//   workboxConfig = workboxConfig || defaultInjectConfig;
+//   return function rewireWorkboxInner(config, env) {
+//     removeSWPrecachePlugin(config);
+//     // Add the Workbox plugin
+//     config.plugins.push(new workboxPlugin.InjectManifest(workboxConfig));
+//     return config;
+//   };
+// }
 
 module.exports = function override(config, env) {
   if (env === 'production') {
     console.log('Production build - Adding Workbox for PWAs');
-    // Extend the default injection config with required swSrc
-    const workboxConfig = {
-      ...defaultInjectConfig,
-      swSrc: path.join(__dirname, 'src', 'custom-sw.js')
-    };
-    config = rewireWorkboxInject(workboxConfig)(config, env);
+    removeSWPrecachePlugin(config);
+    config.plugins.push(
+      new InjectManifest({
+        swSrc: path.join(__dirname, 'src', 'service-worker.js')
+      })
+    );
   }
   return config;
 };
